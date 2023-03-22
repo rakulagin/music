622e04c175e3bc7185cee0512849b2a44370dba3
rakulagin <koulagin@gmail.com>
2023-03-22 21:31:51 +0400

    добавил отображение общего времени аудио

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import React, {useState, useRef, useEffect} from "react";
+import React, {useState, useRef} from "react";
 import './App.css';
 import song from './audio/PARADISE (COLDPLAY) OK..mp3'
 
 function App() {
 
   const myRef = useRef(null)
-
   const [currentTime, setCurrentTime] = useState('00:00')
 
   const play = () => {
@@ -15,25 +14,30 @@ function App() {
     myRef.current.pause()
   }
 
-  const handleTimeUpdate = () => {
-    const time = myRef.current.currentTime
+  // хелпер перевода времени в красивое значение, получает на вход 1.540950495, возвращает 01:00
+  const formatTime = (time) => {
+    if (!time) return
     const minutes = Math.floor(time / 60);
     const seconds = Math.floor(time % 60);
     const formatMinutes = minutes < 10 ? `0${minutes}` : minutes
     const formatSeconds = seconds < 10 ? `0${seconds}` : seconds;
-    setCurrentTime(`${formatMinutes}:${formatSeconds}`)
+    return (`${formatMinutes}:${formatSeconds}`)
+  }
+
+  // функция обновления отображения времени
+  const handleTimeUpdate = () => {
+    const time = myRef.current.currentTime
+    setCurrentTime(formatTime(time))
   };
-  
 
-
-
-
+  // без знака вопроса сюда ничего не приходит и код выпадает в ошибку
+  const duration = formatTime(myRef.current?.duration)
 
   return (
     <div className="App">
       <header className="App-header">
         <p>player</p>
-        <p>{currentTime}</p>
+        <p>{currentTime} / {duration}</p>
         <audio ref={myRef} onTimeUpdate={handleTimeUpdate} controls src={song}></audio>
         <button onClick={play}>play</button>
         <button onClick={stop}>stop</button>
