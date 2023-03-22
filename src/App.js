701a1e8b28d2d9feb83e718ac2bd78944a5a5772
rakulagin <koulagin@gmail.com>
2023-03-22 19:57:10 +0400

    тестирую play/pause

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
-import logo from './logo.svg';
+import React, {useRef} from "react";
 import './App.css';
 import song from './audio/PARADISE (COLDPLAY) OK..mp3'
 
 function App() {
+
+  const myRef = useRef()
+
+  const play = () => {
+    myRef.current.play()
+  }
+
+  const stop = () => {
+    myRef.current.pause()
+  }
 
   console.log(song)
 
@@ -10,7 +20,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <p>player</p>
-        <audio controls src={song}></audio>
+        <audio ref={myRef} controls src={song}></audio>
+        <button onClick={play}>play</button>
+        <button onClick={stop}>stop</button>
       </header>
     </div>
   );
