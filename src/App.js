de86f869e3f41bc4bf426dac6da1e0ca54631ae1
rakulagin <koulagin@gmail.com>
2023-04-20 19:24:40 +0400

    временно решил проблему с перемоткой назад по песне

--- src/App.js
+++ src/App.js
@@ -37,14 +37,21 @@ function App() {
   const duration = isLoaded ? formatTime(songRef.current.duration) : '00:00'
 
   const changeCurrentTime = (event)=> {
-    const progressBarWidth = event.target.offsetWidth
+    // Тут проблема. если кликнуть на голубую область (отмотать назад)
+    // То, offsetWidth считается не по всей белой полоске, а по голубой
+    // const progressBarWidth = event.target.offsetWidth
+    const progressBarWidth = 650
     const click = event.clientX - event.target.offsetLeft
     const percentClicked = (click / progressBarWidth) * 100
     const newTime = songRef.current.duration / 100 * percentClicked
 
     songRef.current.currentTime = newTime
     
-    console.log(newTime)
+    console.log('newTime', newTime)
+    console.log('progressBarWidth', progressBarWidth)
+    console.log('click', click)
+    console.log('percentClicked', percentClicked)
+    console.log('=====')
   }
 
   return (
