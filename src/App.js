a1069a61569d6c8ba495f926e1acc84d54d5a5e4
rakulagin <koulagin@gmail.com>
2023-05-03 22:32:47 +0400

    добавил кнопки
    вперед, назад, мьют

--- src/App.js
+++ src/App.js
@@ -1,29 +1,22 @@
 import React, {useState, useRef} from "react";
 import './App.css';
+import {songsList} from "./audio/audio";
 
-
-import song1 from './audio/PARADISE (COLDPLAY) OK..mp3'
-import song2 from './audio/01. Тишина ванной комнаты (feat. My Automata).mp3'
-import song3 from './audio/02. Случайности (feat. My Automata).mp3'
-import song4 from './audio/03. Душевное состояние в ожидании чего-то хорошего (feat. My Automata).mp3'
-import song5 from './audio/04. Первая Любовь (feat. My Automata).mp3'
-import song6 from './audio/05. Характеры (feat. My Automata).mp3'
 
 function App() {
-  const [songs, setSongs] = useState([song1, song2, song3, song4, song5, song6]);
+  const [songs, setSongs] = useState(songsList);
+  const [numberSongs, setNumberSongs] = useState(songsList.length);
+  const [currentSongIndex, setCurrentSongIndex] = useState(0);
+
   const audioRef = useRef(null)
   const [isLoaded, setIsLoaded] = useState(false)
   const [currentDisplayTime, setCurrentDisplayTime] = useState('00:00')
   const [progressWidth, setProgressWidth] = useState(0)
+  const [volume, setVolume] = useState(1);
 
   const handleLoadAudio = (event)=> {
     setIsLoaded(true)
-
   }
-
-
-
-
 
   const play = () => {
     audioRef.current.play()
@@ -31,10 +24,29 @@ function App() {
   const pause = () => {
     audioRef.current.pause()
   }
+  const stop = ()=> {
+    audioRef.current.pause()
+    audioRef.current.currentTime=0
+  }
+  const next = () => {
+    if(currentSongIndex+1 < songsList.length) {
+      setCurrentSongIndex(currentSongIndex+1)
+    } else {
+      setCurrentSongIndex(0)
+    }
+  }
+  const prev = ()=> {
+    if (currentSongIndex === 0) {
+      console.log('000')
+      setCurrentSongIndex(songsList.length-1)
+    } else {
+      setCurrentSongIndex(currentSongIndex-1)
+    }
+  }
 
   // хелпер перевода времени в красивое значение, получает на вход 1.540950495, возвращает 01:00
   const formatTime = (time) => {
-    if (!time) return
+    if (!time) return '00:00'
     const minutes = Math.floor(time / 60);
     const seconds = Math.floor(time % 60);
     const formatMinutes = minutes < 10 ? `0${minutes}` : minutes
@@ -50,6 +62,19 @@ function App() {
     setProgressWidth(percentProgress)
   };
 
+  const handleVolumeChange = (event)=> {
+    // setVolume(event.target.value);
+    // audioRef.current.volume = 0
+  }
+
+  const mute = () => {
+    if (audioRef.current.volume === 0) {
+      audioRef.current.volume = 1
+    } else {
+      audioRef.current.volume = 0
+    }
+  }
+
   const duration = isLoaded ? formatTime(audioRef.current.duration) : '00:00'
 
   const changeCurrentTime = (event)=> {
@@ -63,22 +88,43 @@ function App() {
     audioRef.current.currentTime = newTime
   }
 
+  // console.log('songsList.length', songsList.length)
+  // console.log('currentSong', currentSongIndex)
+  // console.log('now playing---', songsList[currentSongIndex].artist, songsList[currentSongIndex].title)
+  // console.log('currentDisplayTime', currentDisplayTime)
+  console.log('volume', volume)
+
   return (
     <div className="App">
       <header className="App-header">
         <p>player</p>
+        <p>{songsList[currentSongIndex].artist} - {songsList[currentSongIndex].title}</p>
         <p>{currentDisplayTime} / {duration}</p>
         <audio ref={audioRef}
+               autoPlay={true}
+               // onVolumeChange={handleVolumeChange}
                onLoadedMetadata={handleLoadAudio}
                onTimeUpdate={handleTimeUpdate}
-               controls
-               src={song2}>
+               // controls
+               src={songsList[currentSongIndex].src}>
         </audio>
         <div className='progressBar' onClick={changeCurrentTime}>
           <div className='progress' style={{width: `${progressWidth}%`}}></div>
         </div>
         <button onClick={play}>play</button>
         <button onClick={pause}>pause</button>
+        <button onClick={stop}>stop</button>
+        <button onClick={next}>next</button>
+        <button onClick={prev}>prev</button>
+        <button onClick={mute}>mute</button>
+        <input
+          type="range"
+          min="0"
+          max="1"
+          step="0.1"
+          value={volume}
+          onChange={handleVolumeChange}
+        />
       </header>
     </div>
   );
---
+++ src/audio/audio.js
@@ -0,0 +1,51 @@
+import song1 from '../audio/PARADISE (COLDPLAY) OK..mp3'
+import song2 from '../audio/Макс Корж - Малый повзрослел ч.2 (2017)/1. Макс Корж - Вспоминай меня.mp3'
+import song3 from '../audio/Макс Корж - Малый повзрослел ч.2 (2017)/2. Макс Корж - Молодость всё прощает.mp3'
+import song4 from '../audio/Макс Корж - Малый повзрослел ч.2 (2017)/3. Макс Корж - Пьяный дождь.mp3'
+import song5 from '../audio/Макс Корж - Малый повзрослел ч.2 (2017)/4. Макс Корж - Малиновый закат.mp3'
+import song6 from '../audio/Макс Корж - Малый повзрослел ч.2 (2017)/5. Макс Корж - Оптимист.mp3'
+import song7 from '../audio/2013 - Жить в кайф/07. Жить в кайф.mp3'
+import song8 from '../audio/2013 - Жить в кайф/12. Тает дым.mp3'
+
+export const songsList = [
+  {
+    src: song1,
+    artist: 'Coldplay',
+    title: 'Paradise',
+  },
+  {
+    src: song2,
+    artist: 'Макс Корж',
+    title: 'Вспоминай меня',
+  },
+  {
+    src: song3,
+    artist: 'Макс Корж',
+    title: 'Молодость всё прощает',
+  },
+  {
+    src: song4,
+    artist: 'Макс Корж',
+    title: 'Пьяный дождь',
+  },
+  {
+    src: song5,
+    artist: 'Макс Корж',
+    title: 'Малиновый закат',
+  },
+  {
+    src: song6,
+    artist: 'Макс Корж',
+    title: 'Оптимист',
+  },
+  {
+    src: song7,
+    artist: 'Макс Корж',
+    title: 'Жить в кайф',
+  },
+  {
+    src: song8,
+    artist: 'Макс Корж',
+    title: 'Тает дым',
+  },
+]
